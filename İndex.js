a76ea67bb68a6cc347d94de168c73c19895e56fb
omer567999 <o8183419@gmail.com>
2026-01-09 09:37:43 +0300

    Add Index.js with user interaction and functions
    This commit adds a JavaScript file with various functions, conditional statements, and console logs for user interaction and data manipulation.

---
+++ İndex.js
@@ -0,0 +1,152 @@
+let name = "ömer";
+console.log("name");
+let age = "12";
+if(age>=17) {
+console.log("girebilirsin");
+    
+}else {
+console.log("giremezsin");
+    
+}
+let name = "ömer";
+let age = 17;
+let goal = "soıftvare";
+console.log("hello, my name is "+name+"ı'am"+age+"years old my goals is:"+goal);
+let favorite = "beyzboıol";
+let surname = "özvariş";
+let name = "ömer";
+console.log("this is best"+favorite+"my surname"+surname+"first name:"+name);
+let age = number(prompot("enter your age"));
+
+if(age >= 17) {
+console.log("you are an adult");
+   }else {
+    console.log("you are not adult");
+}
+let grade = number(prompot("enter your grade")):
+if(grade >= 18) {
+    console.log("a");
+}else {
+    console.log("b");
+}
+let person = {
+    name:"ömer",
+    age:"17",
+    favoriye:"software",
+};
+console.log(person.name);
+console.log(person.age);
+function selamla(isim) {
+   console.log("merhaba"+isim);
+}
+selamla("ömer");
+
+function cikart(a,b) {
+    console.log(a-b);
+}
+çıkart(5,6);
+
+let shopinglist = [];
+shopinglist.push("bread");
+shopinglist.push("milk");
+shopinglist.push("chese");
+console.log(shopinglist);
+
+
+let password = "1907";
+let input = prompot("Enter pasword");
+
+if(input=== pasword) {
+    console.log("login sucerful");
+}else {
+    console.log("wrong pasword");
+    
+}
+let frutis = ["banana","Apple","orange"];
+frutis.push["chery"];
+console.log(frutis);
+
+let frutis = ["apple","banana"];
+frutis.remove["apple"];
+console.log(frutis);
+
+let brithyear =number (prompot("enter your  brithyear"));
+let curentyear = 2026 ;
+let age = brithyear-curentyear;
+console.log("your is age :"+age); 
+function hi(name) {
+    console.log("hello"+name);
+}
+hi("ömer")
+
+
+let score = number(prompot("enter your score"));
+if(score >= 90) {
+    console.log("grade A");
+}else if (score >= 70) {
+    console.log("grade B");
+}else if (score>= 50){
+    console.log("grade C");
+}else{
+     console.log("you  difeld");       
+}  
+   
+let users = [
+    {name:"ömer",age:17},
+    {name:"jack",age:18},
+    {name:"lil",age:19}
+];            
+console.log(users[0].name);            
+
+for(let i = 1: i <=10:i++){
+    if(i%2 == 0) {
+        console.log("even : "+i);
+    }
+}
+for(let i=1: i<=19:i++){
+    if(i%2===0) {
+        console.log("sonuc: " +i);
+    }
+}
+let languges =["javascript","python","c"];
+for(let i = 0:i < languges.length:i++){
+    console.log(languges[i]);
+}
+
+function clicknumber(number) {
+    if(number%2===0) {
+        console.log("even");
+    }else {
+        console.log("odd");
+    }
+}
+clickNumber(7);
+
+function clickNumber(num) {
+    if(num%2===0) {
+        console.log("even");
+    }else {
+        console.log("odd");
+    }
+}
+clickNumber(555)
+
+
+let name = prompot("what is your name");
+let agae = number(prnompot("how old are you"));
+
+if(age < 18) {
+    console.log("access denied");
+}else {
+    console.log("welcome:" + name);
+}
+
+function iseven (nummber) {
+    return number%2===0;
+}
+console.log(iseven(4));
+
+function isadult(age) {
+    return age>=18;
+}
+console.log(isadult(88));
